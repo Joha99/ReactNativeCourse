4a53c32f69e8056d3bf33b4718207d256a242e7e
Joha Kim <johakim9936@gmail.com>
2022-01-28 13:57:13 -0500

    add a user documents to firestore when they register

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -3,22 +3,21 @@ import { AppForm, AppFormField, SubmitButton } from "../components/forms/index";
 import { StyleSheet } from "react-native";
 import Screen from "../components/Screen";
 import * as Yup from "yup";
-import { auth } from "../../firebase/firebase";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-} from "firebase/auth";
+import { auth, db } from "../../firebase/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { collection, addDoc } from "firebase/firestore";
 
 // validation schema
 const validationSchema = Yup.object().shape({
-  name: Yup.string().required().label("Name"),
+  firstName: Yup.string().required().label("First Name"),
+  lastName: Yup.string().required().label("Last Name"),
   email: Yup.string().required().email().label("Email"),
   password: Yup.string().required().min(4).label("Password"),
 });
 
 export default function RegisterScreen() {
-  const handleRegister = (email, password) => {
+  const handleRegister = async (firstName, lastName, email, password) => {
+    // create user in firebase authentication
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
@@ -30,21 +29,44 @@ export default function RegisterScreen() {
         console.log("Error registering", errorMessage);
         alert("Unsuccessful register");
       });
+
+    //create user in firestore
+    const docRef = await addDoc(collection(db, "users"), {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+    })
+      .then((docRef) => alert("Added user in Firestore with ID", docRef.id))
+      .catch((error) => {
+        const errorMessage = error.message;
+        alert("Unsuccessful at adding user to Firestore", errorMessage);
+      });
+
+    console.log("Document written with ID: ", docRef.id);
   };
 
   return (
     <Screen style={styles.container}>
       <AppForm
-        initialValues={{ name: "", email: "", password: "" }}
-        onSubmit={({ email, password }) => handleRegister(email, password)}
+        initialValues={{ firstName: "", lastName: "", email: "", password: "" }}
+        onSubmit={({ firstName, lastName, email, password }) =>
+          handleRegister(firstName, lastName, email, password)
+        }
         validationSchema={validationSchema}
       >
         <AppFormField
-          placeholder="Name"
+          placeholder="First Name"
           icon="account"
           autoCapitalize="none"
           autoCorrect={false}
-          name="name"
+          name="firstName"
+        />
+        <AppFormField
+          placeholder="Last Name"
+          icon="account"
+          autoCapitalize="none"
+          autoCorrect={false}
+          name="lastName"
         />
         <AppFormField
           placeholder="Email"
@@ -64,7 +86,7 @@ export default function RegisterScreen() {
           secureTextEntry
           name="password"
         />
-        <SubmitButton title="Login" />
+        <SubmitButton title="Register" />
       </AppForm>
     </Screen>
   );
